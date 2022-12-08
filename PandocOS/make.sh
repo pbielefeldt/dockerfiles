27347f6b6bf0a41576f8dae3a8b865900223773d
Philipp Bielefeldt <bielefeldt@uni-bonn.de>
2022-12-08 17:52:45 +0100

    Update make.sh

--- PandocOS/make.sh
+++ PandocOS/make.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+#sudo systemctl start docker
 TAGNAME="pbielefeldt/pandocos:test"
 
 docker build --platform linux/amd64 --tag ${TAGNAME} .
 
 docker login
 docker push ${TAGNAME}
-
