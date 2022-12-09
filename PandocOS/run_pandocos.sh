fa4a599ab5b5b0a85b1e679f6486f3d3de447977
Bielefeldt <philipp.bielefeldt@4soft.de>
2022-12-09 10:28:30 +0100

    new runfile

--- PandocOS/run_pandocos.sh
+++ PandocOS/run_pandocos.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #sudo systemctl start docker
 
+export SHARED_DIR="${HOME}/DockerShared"
+export DOCKER_TAG="pbielefeldt/pandocos:latest"
+
+#if [[ ! -d ${SHARED_DIR} ]]; then
+#  mkdir -p ${SHARED_DIR}
+#fi
+echo -e "mounting $SHARED_DIR as /mnt/shared"
+
 docker run --rm -i -t \
   --name my_pandoc \
-  -v ~/Sammlung/Dockers/SharedVolume/:/mnt/shared \
-  pbielefeldt/pandocos:test
-
+  -v ${SHARED_DIR}:/mnt/shared \
+  ${DOCKER_TAG}
