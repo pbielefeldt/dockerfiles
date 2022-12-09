8216c27c1747c9bd0e07b55b9cf16a1121a4c840
Bielefeldt <philipp.bielefeldt@4soft.de>
2022-12-09 10:17:12 +0100

    rm fmutil (broken?)

--- PandocOS/make.sh
+++ PandocOS/make.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #sudo systemctl start docker
-TAGNAME="pbielefeldt/pandocos:test"
+TAGNAME="pbielefeldt/pandocos:latest"
 
 docker build --platform linux/amd64 --tag ${TAGNAME} .
 
