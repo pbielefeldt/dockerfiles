f56548e3cde7d95f2f7464f9e6dc3861d869c738
Philipp Bielefeldt <p.bielefeldt@mailbox.org>
2024-08-27 22:11:39 +0200

    set image to Leap 15.6

--- LaTexOS/make.sh
+++ LaTexOS/make.sh
@@ -2,9 +2,10 @@
 
 #sudo systemctl start docker
 TAGNAME="pbielefeldt/latexos"
-TAGVERSION="15.5.4"
+TAGVERSION="15.6.1"
 
 docker build --platform linux/amd64 --tag ${TAGNAME}:${TAGVERSION} --tag ${TAGNAME}:latest .
 
 #docker login
 docker push ${TAGNAME} --all-tags
+
